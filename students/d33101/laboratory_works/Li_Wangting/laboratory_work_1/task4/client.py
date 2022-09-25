6300c027dbf3e758a109518238c3519fff1bbb30
Ли Вантин <88771809+Volta44@users.noreply.github.com>
2022-09-26 00:00:15 +0300

    Add files via upload

--- students/d33101/laboratory_works/Li_Wangting/laboratory_work_1/task4/client.py
+++ students/d33101/laboratory_works/Li_Wangting/laboratory_work_1/task4/client.py
@@ -1,1 +1,34 @@
+import socket
+import threading
 
+nickname = input("Nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 5500))
+print('Connection to server...')
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode()
+            if message == 'NICK':
+                client.send(nickname.encode())
+            else:
+                print(message)
+        except:
+            break
+        
+def write():
+    while True:
+        message = input('')
+        if message == 'exit':
+            print('Break connetion...')
+            client.close()
+            break
+        client.send(message.encode())
+
+receive_thread = threading.Thread(target = receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target = write)
+write_thread.start()
